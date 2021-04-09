258ceb105c7d40cd6e57e03c4f592001fe010a07
Eva <yi.zheng1709@gmail.com>
2021-04-09 02:16:34 -0400

    commit tree fb1278e03933fdd79e2a2f50150f8234ce91a6da

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Write your code here!
+document.getElementById("main").remove();
+let h1 = document.createElement("h1")
+document.body.appendChild(h1) 
+h1.id = "victory"
+// h1.id = "victory"
+let newHeader = document.querySelector("h1#victory")
+newHeader.innerHTML = "No one is the champion"
